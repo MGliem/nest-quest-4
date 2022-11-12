a6f3e4483ef5e9dd073c7b5e7c23ade7a6106c1d
MGliem <maximilian.gliem@gmail.com>
2022-11-12 16:14:48 +0100

    jwt constant strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { jwtConstants } from './constants';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService],
+  imports: [UsersModule, JwtModule.register({ secret: jwtConstants.secret })],
+  providers: [UsersService, AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
